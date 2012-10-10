2df1f9657547524e2c6c59054f760c08e56dfc1a
Gianni Gambetti <Gianni@Gambetti.ca>
2012-10-10 00:29:40 -0500

    Updated About page and associated parts

--- js/view/AboutView.js
+++ js/view/AboutView.js
@@ -2,8 +2,10 @@ define([
 	'lib/ember',
 	'view/PageTemplateView',
 	'mixin/templateLoader',
-	'mixin/navtab_view'
-],function( Ember, templateView, templateLoader, navtab_view ){
+	'mixin/navtab_view',
+	'lib/jquery',
+	'lib/jquery.timeago'
+],function( Ember, templateView, templateLoader, navtab_view, $, timeago ){
 	
 	var view = templateView.extend( navtab_view, {
 		heading: 'Gianni Gambetti',
@@ -14,7 +16,14 @@ define([
 			classNames: [ 'about' ],
 			templateUrl: '/template/about.html',
 			contentBinding: Ember.Binding.oneWay('parentView.content')
-		})
+		}),
+
+		didInsertElement:function(){
+			this._super();
+
+			$('abbr.timeago').timeago();
+
+		}
 
 	});
 
