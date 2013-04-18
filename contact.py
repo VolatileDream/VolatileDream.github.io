482b4991978cc16f16cc70e0510b88622d43d181
Gianni Gambetti <Gianni@Gambetti.ca>
2013-04-18 19:24:11 -0400

    Made all links + media load over https

--- contact.py
+++ contact.py
@@ -7,30 +7,30 @@ data = site.get_base_data();
 data['contacts'] = [
 	{
 		"logo":{
-			"url":"//ssl.gstatic.com/images/icons/gplus-64.png",
+			"url":"https://ssl.gstatic.com/images/icons/gplus-64.png",
 			"width":"64px",
 			"height":"64px"
 		},
 		"tagline":"on Google+",
-		"link":"//plus.google.com/113067837896503448428"
+		"link":"https://plus.google.com/113067837896503448428"
 	},
 	{
 		"logo": {
-			"url":"//twitter.com/images/resources/twitter-bird-light-bgs.png",
+			"url":"https://twitter.com/images/resources/twitter-bird-light-bgs.png",
 			"width":"64px",
 			"height":"64px"
 		},
 		"tagline":"@volatiledream on Twitter",
-		"link":"//twitter.com/volatiledream"
+		"link":"https://twitter.com/volatiledream"
 	},
 	{
 		"logo": {
-			"url":"//developer.linkedin.com/sites/default/files/LinkedIn_Logo60px.png",
+			"url":"https://developer.linkedin.com/sites/default/files/LinkedIn_Logo60px.png",
 			"width":"64px",
 			"height":"100%"
 		},
 		"tagline":"on Linkedin",
-		"link":"//www.linkedin.com/in/giannigambetti"
+		"link":"https://www.linkedin.com/in/giannigambetti"
 	},
 	{
 		"logo": {
@@ -43,21 +43,21 @@ data['contacts'] = [
 	},
 	{
 		"logo":{
-			"url":"//newsroom.fb.com/download-media/265/1",
+			"url":"https://newsroom.fb.com/download-media/265/1",
 			"width":"64px",
 			"height":"64px"
 		},
 		"tagline":"on facebook",
-		"link":"//facebook.com/gambetti"
+		"link":"https://facebook.com/gambetti"
 	},
 	{
 		"logo":{
-			"url":"//octodex.github.com/images/original.jpg",
+			"url":"https://octodex.github.com/images/original.jpg",
 			"width":"64px",
 			"height":"64px"
 		},
 		"tagline":"on github",
-		"link":"//github.com/jex"
+		"link":"https://github.com/jex"
 	}
 ];
 
