f99a142f446feceb2083ddc8dc7f2663f08977b1
Gianni Gambetti <Gianni@Gambetti.ca>
2013-06-25 23:01:28 -0400

    Added update (post) time to blog posts

--- db/BlogPost.py
+++ db/BlogPost.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base();
 
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, Sequence, DateTime
+
+import datetime
 
 class BlogPost(Base):
 
@@ -12,11 +14,13 @@ class BlogPost(Base):
 	id = Column(Integer, Sequence('blog_post_id_seq'), primary_key=True);
 	title = Column("title", String);
 	content = Column("content", String);
+	updated = Column("updated", DateTime, default=datetime.datetime.utcnow);
 
 	def __init__(self, id, title, content):
 		self.id = id;
 		self.title = title;
 		self.content = content;
+		self.updated = datetime.datetime.utcnow();
 
 	def __repr__(self):
-		return "<BlogPost('%s','%s','%s)>" % (self.id, self.title, self.content);
+		return "<BlogPost('%s','%s','%s','%s')>" % (self.id, self.title, self.content, self.updated);
